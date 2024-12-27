12265540fe54f533f5a535789c4cede6892baca0
Samsonidze <119052018+Samsonidze@users.noreply.github.com>
2024-12-27 19:09:52 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
 
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
+
+    if(response.status == 404){
+        window.alert("Please Enter the city name correctly")
+    }else{
 
     console.log(data)
 
@@ -16,9 +21,21 @@ async function checkWeather(city){
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png"
+    } else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png"
+    } else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png"
+    } else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png"
+    } else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png"
+    } 
+
 
 }
-
+}
 
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
